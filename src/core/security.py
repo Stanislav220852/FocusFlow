78498bd3dad7c47297b161313b4843f19c6d2773
Stas <stasfomcenko5@gmail.com>
2025-12-29 19:30:35 +0300

    refactoring core

--- src/core/security.py
+++ src/core/security.py
@@ -26,7 +26,7 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 
 
-def create_access_token(data: dict):
+def create_access_token(data: dict)-> str:
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
