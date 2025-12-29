73acef335f3d5e0e602c23d31ae32b09228d36ac
Stas <stasfomcenko5@gmail.com>
2025-12-29 18:55:46 +0300

    refactoring dependencies.py

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,4 +1,5 @@
 import jwt
+from typing import Annotated
 from fastapi import Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
@@ -8,27 +9,43 @@ from src.models.user import User
 from src.core.config import settings
 from src.core.security import oauth2_scheme
 
-async def get_current_user(
-    token: str = Depends(oauth2_scheme), 
-    db: AsyncSession = Depends(get_db)
-):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credential",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
+
+
+DBSessionDep = Annotated[AsyncSession, Depends(get_db)]
+TokenDep = Annotated[str, Depends(oauth2_scheme)]
+
+
+CREDENTIALS_EXCEPTION = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+async def get_current_user(token: TokenDep, db: DBSessionDep) -> User:
+   
     try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+       
+        payload = jwt.decode(
+            token, 
+            settings.SECRET_KEY, 
+            algorithms=[settings.ALGORITHM]
+        )
         email: str = payload.get("sub")
         if email is None:
-            raise credentials_exception
+            raise CREDENTIALS_EXCEPTION
+            
     except jwt.PyJWTError:
-        raise credentials_exception
+        raise CREDENTIALS_EXCEPTION
 
+  
     query = select(User).where(User.email == email)
     result = await db.execute(query)
     user = result.scalar_one_or_none()
     
     if user is None:
-        raise credentials_exception
+        raise CREDENTIALS_EXCEPTION
+        
     return user
+
+
+CurrentUserDep = Annotated[User, Depends(get_current_user)]
