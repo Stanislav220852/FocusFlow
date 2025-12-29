135c99cdd82fab331a21431929351d7c4a0a2d55
Stas <stasfomcenko5@gmail.com>
2025-12-29 18:49:25 +0300

    Refactoring main.py

--- main.py
+++ main.py
@@ -1,45 +1,48 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 from src.core.config import settings
-import uvicorn
+from src.db.redis import init_redis, close_redis
 from src.api.users import user_router
 from src.api.rooms import room_router
-from src.api.dependencies import get_current_user
-from src.models.user import User
-from contextlib import asynccontextmanager
-from src.db.redis import init_redis, close_redis
 from src.api.focus_session import focus_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-   
     await init_redis()
     yield
-    
     await close_redis()
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     version=settings.VERSION,
     lifespan=lifespan
-    
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(user_router)
 app.include_router(room_router)
 app.include_router(focus_router)
 
-@app.get("/healthcheck")
-async def health_check(current_user: User = Depends(get_current_user)):
+
+@app.get("/healthcheck", tags=["System"])
+async def health_check():
     return {
         "status": "ok",
-        "project_name": settings.PROJECT_NAME,
-        "version": settings.VERSION
+        "project": settings.PROJECT_NAME,
+        "version": settings.VERSION,
+        "debug_mode": settings.DEBUG
     }
-    
 
-    
 if __name__ == "__main__":
-    uvicorn.run("main:app",reload=True)
-    
-    
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
