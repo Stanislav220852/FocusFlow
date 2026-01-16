20a2b7773a44978e6d868773efb089466aedb17c
Stanislav220852 <stasfomcenko5@gmail.com>
2026-01-16 19:32:29 +0300

    add test test_healthcheck

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,5 @@ from main import app
 
 @pytest.fixture(scope="session")
 async def ac():
-    """Создает асинхронный клиент для тестов"""
-    # ASGITransport позволяет общаться с FastAPI напрямую в памяти
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
         yield client
---
+++ tests/test_healthcheck.py
@@ -0,0 +1,10 @@
+import pytest
+from httpx import AsyncClient
+
+@pytest.mark.asyncio
+async def test_health_check(ac: AsyncClient):
+  
+    response = await ac.get("/healthcheck") 
+    
+    assert response.status_code == 200
+    assert response.json()["status"] == "ok"
