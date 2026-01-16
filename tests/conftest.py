939c8f1da9785f6228776fc4b97b80b17ac28a55
Stanislav220852 <stasfomcenko5@gmail.com>
2026-01-17 01:05:56 +0300

    add fixture and test /users/get_me

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,37 @@
 import pytest
 from httpx import AsyncClient, ASGITransport
 from main import app 
+import uuid
 
 @pytest.fixture(scope="session")
 async def ac():
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
         yield client
+
+
+@pytest.fixture(scope= "function")
+async def token(ac:AsyncClient) -> str:
+    unique_id = uuid.uuid4().hex[:6]
+    unique_email = f"user_{unique_id}@gmail.com"
+    unique_username = f"test_user_{unique_id}" 
+    login_password = "Fish123@"
+    login_username = "Stas"
+   
+   
+
+    await ac.post("/users/register",json = {
+        "username": unique_username,
+        "email": unique_email,
+        "password": login_password
+    })
+    
+
+    login = await ac.post("/users/login",data = {
+        "username": login_username,
+        "password": login_password
+    })
+    assert login.status_code == 200
+    return login.json()["access_token"]
+  
+    
+
--- tests/test_users.py
+++ tests/test_users.py
@@ -3,16 +3,10 @@ import pytest
 import uuid
 
 @pytest.mark.asyncio
-async def test_register(ac:AsyncSession):
-    unique_email = f"user_{uuid.uuid4().hex[:6]}@gmail.com"
-    payload = {
-        "username": f"test_user_{uuid.uuid4().hex[:6]}",
-        "email": unique_email,
-        "password": "Password123!"
-    }
-
-    response = await ac.post("/users/register",json = payload)
-
-    assert response.status_code == 200
+async def test_get_me(ac:AsyncClient,token:str):
     
-
+    headers = {"Authorization": f"Bearer {token}"}
+    response = await ac.get("/users/me", headers=headers)
+    assert response.status_code == 200
+    data = response.json()
+    print(data)
